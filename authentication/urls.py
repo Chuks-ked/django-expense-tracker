7dcfb33d556d1b6fa04ef64cca74d000b0e2e939
Chuks <ikedichukwubuikem@gmail.com>
2023-03-23 08:22:00 +0100

    integrated

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,9 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
     path('validate-username/',csrf_exempt(views.UsernameValidation.as_view()), name='validate-username'),
 
     path('validate-email/', csrf_exempt(views.EmailValidation.as_view()), name='validate_email'),
+
+    path('acitvate/<uidb64>/<token>/', views.VerificationView.as_view(), name='activate')
     
 ]
