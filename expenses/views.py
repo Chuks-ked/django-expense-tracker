8d5c4afdd3e33f2d137cd7221bff1eb446b954a7
Chuks <ikedichukwubuikem@gmail.com>
2023-04-24 00:46:22 +0100

    pdf initials

--- expenses/views.py
+++ expenses/views.py
@@ -10,6 +10,10 @@ from userpreferences.models import UserPreference
 import datetime
 import csv
 import xlwt
+from django.template.loader import render_to_string
+from weasyprint import HTML
+import tempfile
+from django.db.models import Sum
 # Create your views here.
 
 def search_expenses(request):
@@ -184,4 +188,24 @@ def export_excel(request):
             ws.write(row_num, col_num, str(row[col_num]), font_style)
 
     wb.save(response)
+    return response
+
+def export_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition']='attachment; filename=Expenses' + str(datetime.datetime.now()) + '.pdf'
+
+    response['Content-Transfer-Encoding'] = 'binary'
+
+    html_string=render_to_string('expenses/pdf-output.html', {'expenses':[], 'total':0})
+    html=HTML(string=html_string)
+
+    result = html.write_pdf()
+
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+
+        output = open(output.name, 'rb')
+        response.write(output.read())
+
     return response
